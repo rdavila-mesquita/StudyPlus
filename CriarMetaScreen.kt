7ef66b927fff34501ea273f1460fc6a47087b0a5
Dávila Ramos de Mesquita <davilaramos9987@gmail.com>
2025-12-08 17:50:46 -0300

    tela para criar uma nova meta

---
+++ CriarMetaScreen.kt
@@ -0,0 +1,186 @@
+package com.example.study.activity
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun CriarMeta(
+    onSaveMeta: (String, String) -> Unit = {_, _ -> },
+    onHomeClick: () -> Unit = {},
+    onMetaClick: () -> Unit = {}
+) {
+
+    var titulo by remember { mutableStateOf("") }
+    var assunto by remember { mutableStateOf("") }
+
+    Scaffold(
+        bottomBar = { BottomBar(onHomeClick = onHomeClick, onMetaClick = onMetaClick) }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF2E3F9D))
+                .padding(innerPadding)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 30.dp)
+                    .padding(top = 80.dp)
+                    .clip(shape = RoundedCornerShape(15.dp))
+                    .background(Color.White)
+
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+
+                    Icon(
+                        painter = painterResource(com.example.study.R.drawable.ic_close),
+                        contentDescription = "icone para fechar",
+                        tint = Color(0xFF60C8E9),
+                        modifier = Modifier
+                            .align(Alignment.End)
+                            .size(40.dp)
+                            .clickable(
+                                onClick = onHomeClick
+                            )
+                    )
+
+                    androidx.compose.foundation.Image(
+                        painter = painterResource(com.example.study.R.drawable.profile),
+                        contentDescription = "icone de meta",
+                        modifier = Modifier
+                            .size(80.dp)
+                    )
+
+                    Text(
+                        "Nova Meta",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF2E3F9D),
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(vertical = 10.dp)
+                    )
+
+                    OutlinedTextField(
+                        value = titulo,
+                        onValueChange = { titulo = it },
+                        label = { Text("Título") },
+                        shape = RoundedCornerShape(10.dp),
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(vertical = 10.dp, horizontal = 20.dp)
+                    )
+
+                    OutlinedTextField(
+                        value = assunto,
+                        onValueChange = { assunto = it },
+                        label = { Text("Assunto") },
+                        shape = RoundedCornerShape(10.dp),
+                        modifier = Modifier
+                            .height(300.dp)
+                    )
+
+                    Button(
+                        onClick = { onSaveMeta(titulo, assunto) },
+                        colors = ButtonDefaults.buttonColors(Color(0xFF415DFD)),
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(vertical = 20.dp)
+                            .fillMaxWidth(0.8f)
+                    ) {
+                        Text("Salvar")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    onHomeClick: () -> Unit = {},
+    onMetaClick: () -> Unit = {}
+){
+    BottomAppBar(
+        containerColor = Color(0xFF2E3F9D),
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(com.example.study.R.drawable.ic_home),
+                contentDescription = "icone para tela home",
+                tint = Color(0xFF415DFD),
+                modifier = Modifier
+                    .size(40.dp)
+                    .clickable(
+                        onClick = onHomeClick
+                    )
+            )
+
+
+            IconButton(
+                onClick = onMetaClick
+            ) {
+                Icon(
+                    painter = painterResource(com.example.study.R.drawable.ic_meta),
+                    contentDescription = "icone para tela de meta",
+                    tint = Color.White,
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun CriarMetaPreview(){
+    CriarMeta()
+}
